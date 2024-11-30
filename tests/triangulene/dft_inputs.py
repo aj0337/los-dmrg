f29f1d886e93eb1ee4522369cce0a1fd8a0b6d96
Anooja Jayaraj <Aanoojajayaraj@gmail.com>
2024-11-30 17:31:50 +0100

    Modify gpaw input to allow Hubbard U

--- tests/triangulene/dft_inputs.py
+++ tests/triangulene/dft_inputs.py
@@ -1,25 +1,43 @@
 from __future__ import print_function
-
+import os
 from ase import *
 from ase.io import read
 from gpaw import *
 
+# Load atomic structure
 atoms = read('struct.xyz')
-basis = {'H':'dzp','C':'dzp'}
 
+# Define basis set for atoms
+basis = {'H': 'dzp', 'C': 'dzp'}
+
+U = 11
+hubbard = {'C': ':p,11.0'}
+
+# Define the output directory
+output_dir = f'output/gpaw/HubbardU{U}'
+os.makedirs(output_dir, exist_ok=True)
+
+# Set up GPAW calculator
 calc = GPAW(h=0.2,
             xc='PBE',
             basis=basis,
             occupations=FermiDirac(width=0.2),
             kpts=(1, 1, 1),
             mode='lcao',
-            txt='struct.txt',
+            txt=os.path.join(output_dir, 'struct.txt'),  # Output path for GPAW text output
             mixer=Mixer(0.1, 5, weight=100.0),
-            symmetry={'point_group': False, 'time_reversal': True})
+            symmetry={'point_group': False, 'time_reversal': True},
+            setups=hubbard)
 
 atoms.set_calculator(calc)
-atoms.get_potential_energy()
-calc.write('struct.gpw',mode='all')
 
+# Run calculation
+atoms.get_potential_energy()
+
+# Write the calculator state to a .gpw file in the output directory
+calc.write(os.path.join(output_dir, 'struct.gpw'), mode='all')
+
+# Get and save the Fermi level to a text file in the output directory
 fermi = calc.get_fermi_level()
-print(repr(fermi), file=open('fermi_struct.txt', 'w'))
+with open(os.path.join(output_dir, 'fermi_struct.txt'), 'w') as f:
+    print(repr(fermi), file=f)
